24cfa355b2891edf2d525e994f0956fb78443546
David Volquartz Lebech <david@lebech.info>
2015-05-16 11:50:29 +0200

    Add spectator feature
    This commit implements the spectator feature and ensures that only two players
    can join a game.
    
    Major changes:
    - Add new spectator join functions on both server and client controller.
    - Notify the client if joining as player failed.
    
    Other changes:
    - Added jshint configuration and fixed some errors. Sorry this should have been
      in a separate commit.
    
    Closes #7.

--- src/client/terrain.js
+++ src/client/terrain.js
@@ -1,48 +1,12 @@
-var ImprovedNoise = require('./ImprovedNoise')
+'use strict';
+
+/* global THREE */
+
+var ImprovedNoise = require('./ImprovedNoise');
 
 var terrainWidth = null;
 var terrainHeight = null;
 var yOffset = null;
-
-function Terrain(width, height, offset) {
-	terrainWidth = width * 2;
-	terrainHeight = height * 2;
-	this.yOffset = offset;
-}
-
-Terrain.prototype.getMesh = function() {
-	data = generateHeight( terrainWidth, terrainHeight );
-
-	var geometry = new THREE.PlaneBufferGeometry( 7500, 7500, terrainWidth - 1, terrainHeight - 1 );
-	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
-
-	var vertices = geometry.attributes.position.array;
-
-	for (var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
-		var x = i % terrainWidth, y = ~~ ( i / terrainWidth );
-		if (x > terrainWidth / 4 && x < 3 * terrainWidth / 4 && y > terrainHeight / 4 && y < 3 * terrainHeight / 4) {
-			vertices[j + 1] = yOffset - 50; // flat terrain for the foxy fox!
-		} else {
-			vertices[ j + 1 ] = data[ i ] * 10;			
-		}
-	}
-
-	texture = new THREE.Texture(generateTexture(data), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
-	texture.needsUpdate = true;
-
-	var material = new THREE.MeshBasicMaterial( {
-						color: new THREE.Color().setHSL( 0.3, 0.75, ( i / 15 ) * 0.4 + 0.1 ),
-						map: texture,
-						depthTest: false,
-						depthWrite: false,
-						transparent: true
-					} );
-
-
-	return new THREE.Mesh(geometry, new THREE.MeshBasicMaterial( { map: texture } ));
-}
-
-module.exports = Terrain;
 
 function generateHeight() {
 	var size = terrainWidth * terrainHeight, data = new Uint8Array( size ),
@@ -60,10 +24,10 @@ function generateHeight() {
 
 	return data;
 }
+
 function generateTexture(data) {
 
-	var canvas, canvasScaled, context, image, imageData,
-	level, diff, vector3, sun, shade;
+	var canvas, canvasScaled, context, image, imageData, vector3, sun, shade;
 
 	vector3 = new THREE.Vector3( 0, 0, 0 );
 
@@ -124,4 +88,44 @@ function generateTexture(data) {
 
 	return canvasScaled;
 
-}				
+}
+
+function Terrain(width, height, offset) {
+	terrainWidth = width * 2;
+	terrainHeight = height * 2;
+	this.yOffset = offset;
+}
+
+Terrain.prototype.getMesh = function() {
+  var data = generateHeight( terrainWidth, terrainHeight );
+
+  var geometry = new THREE.PlaneBufferGeometry( 7500, 7500, terrainWidth - 1, terrainHeight - 1 );
+  geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+
+  var vertices = geometry.attributes.position.array;
+
+  for (var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3) {
+    var x = i % terrainWidth, y = ~~ ( i / terrainWidth );
+    if (x > terrainWidth / 4 && x < 3 * terrainWidth / 4 && y > terrainHeight / 4 && y < 3 * terrainHeight / 4) {
+      vertices[j + 1] = yOffset - 50; // flat terrain for the foxy fox!
+    } else {
+      vertices[ j + 1 ] = data[ i ] * 10;
+    }
+  }
+
+  var texture = new THREE.Texture(generateTexture(data), THREE.UVMapping, THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping);
+  texture.needsUpdate = true;
+
+  // var material = new THREE.MeshBasicMaterial( {
+  // 					color: new THREE.Color().setHSL( 0.3, 0.75, ( i / 15 ) * 0.4 + 0.1 ),
+  // 					map: texture,
+  // 					depthTest: false,
+  // 					depthWrite: false,
+  // 					transparent: true
+  // 				} );
+
+
+  return new THREE.Mesh(geometry, new THREE.MeshBasicMaterial( { map: texture } ));
+};
+
+module.exports = Terrain;
