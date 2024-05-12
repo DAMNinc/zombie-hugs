c81543f8af7679ee4c7945fc66a4c883e6e6bbfd
Andreas Schmidt Jensen <andreas@oka.dk>
2024-05-12 17:13:02 +0200

    Added HP to zombies Added offset to better position zombies in gallery

--- src/content/js/gallery.js
+++ src/content/js/gallery.js
@@ -1,13 +1,13 @@
-(function() {
-    var renderAreaId = "render-area",
+(function () {
+    let renderAreaId = "render-area",
         jsonloader = new THREE.JSONLoader(true),
         zombies = {},
         scene = null,
         zombie = null;
-    
+
     function loadModel(name, path) {
         var $deferred = jQuery.Deferred();
-        jsonloader.load(path, function(geo, mat) {
+        jsonloader.load(path, function (geo, mat) {
             var model = {
                 geometry: geo,
                 material: mat
@@ -18,8 +18,8 @@
         return $deferred.promise();
     }
 
-   function setZombie(zombieName) {
-       var newZombie = zombies[zombieName];
+    function setZombie(zombieName) {
+        const newZombie = zombies[zombieName];
 
         function updatePreview() {
             if (zombie) {
@@ -27,12 +27,18 @@
             }
             zombie = new ROME.Animal(newZombie.model.geometry, true);
             zombie.mesh.scale.set(newZombie.previewScale, newZombie.previewScale, newZombie.previewScale);
+
+            
+            zombie.mesh.position.x = newZombie.previewOffset.x;
+            zombie.mesh.position.y = newZombie.previewOffset.y;
+            zombie.mesh.position.z = newZombie.previewOffset.z;
+
             zombie.play(zombie.availableAnimals[0], zombie.availableAnimals[0]);
             zombie.animalA.timeScale = zombie.animalB.timeScale = 0.9;
 
             // Add the mesh of the zombie to the scene.
             scene.add(zombie.mesh);
-        } 
+        }
 
         function updateZombieList() {
             $('a.zombie')
@@ -44,7 +50,7 @@
         function updateFeaturesList() {
             $('.zombie-features')
                 .empty()
-                .append(newZombie.features.reduce(function(features, feature) {
+                .append(newZombie.features.reduce(function (features, feature) {
                     return features + `<li>${feature}</li>`;
                 }, ""));
         }
@@ -52,34 +58,31 @@
         updatePreview();
         updateZombieList();
         updateFeaturesList();
-   }
+    }
 
-   function init() {
+    function init() {
 
         function initPreview() {
             scene = new THREE.Scene();
-            var camera = new THREE.PerspectiveCamera(45, 100 / 100, 1, 10000);
+            const camera = new THREE.PerspectiveCamera(45, 100 / 100, 1, 10000);
 
-            var clock = new THREE.Clock(true);
-            var renderer = new THREE.WebGLRenderer();
-            document.getElementById(renderAreaId).appendChild( renderer.domElement );
-
-            camera.position.y = 25;
-            camera.position.z = 150;
+            const clock = new THREE.Clock(true);
+            const renderer = new THREE.WebGLRenderer();
+            document.getElementById(renderAreaId).appendChild(renderer.domElement);
 
             camera.aspect = renderer.domElement.clientWidth / renderer.domElement.clientHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(renderer.domElement.clientWidth, renderer.domElement.clientHeight);
 
             var render = function () {
-                requestAnimationFrame( render );
+                requestAnimationFrame(render);
 
                 var elapsed = clock.getDelta();
                 if (zombie) {
                     zombie.mesh.rotation.y += 0.01;
-                    zombie.update(elapsed*1000); // TODO Get the fox running
+                    zombie.update(elapsed * 1000);
                 }
-                
+
                 renderer.render(scene, camera);
             };
 
@@ -96,26 +99,26 @@
         <p class="list-group-item-text">${zombie.shortDescription}</p>
     </a>
 </div>` )
-                .appendTo('.zombie-list')
-                .find('a.zombie')
-                .on('click', function() {
-                    setZombie(zombie.name);
-                });
+                    .appendTo('.zombie-list')
+                    .find('a.zombie')
+                    .on('click', function () {
+                        setZombie(zombie.name);
+                    });
 
                 return loadModel(zombie.name, zombie.modelPath);
             }
 
-            return $.getJSON('/content/zombieinfo.json').then(function(data) {
+            return $.getJSON('/content/zombieinfo.json').then(function (data) {
                 return $.when.apply(null, data.zombies.map(initZombie))
             });
         }
 
         initZombies()
-            .then(function() {
+            .then(function () {
                 initPreview();
                 setZombie(Object.getOwnPropertyNames(zombies)[0]);
             })
-   }
-   
-   $(init);
+    }
+
+    $(init);
 })();
